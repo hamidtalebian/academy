95545529e76c3cb9012c952717c11c047f8cbc34
Hamid Talebian <47204986+hamidtalebian@users.noreply.github.com>
2025-08-15 20:17:02 +0330

    Update views.py

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from .models import Course
+#from django.shortcuts import render
+#from .models import Course
+
+#def home(request):
+ #   courses = Course.objects.select_related('teacher').all().order_by('start_date')
+  #  return render(request, 'home.html', {'courses': courses})
+
+
+from django.http import HttpResponse
 
 def home(request):
-    courses = Course.objects.select_related('teacher').all().order_by('start_date')
-    return render(request, 'home.html', {'courses': courses})
+    return HttpResponse("Welcome to Academy Home Page!")
