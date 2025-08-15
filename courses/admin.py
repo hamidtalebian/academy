e5794365b97a3e304dae6cf069299744830bce0b
Hamid Talebian <47204986+hamidtalebian@users.noreply.github.com>
2025-08-15 21:05:33 +0330

    Update admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from .models import Teacher, Course
+from .models import Teacher, Course, Student, Enrollment
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    list_display = ('name', 'expertise')
-    search_fields = ('name', 'expertise')
+    list_display = ('name', 'expertise', 'phone')
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'teacher', 'start_date', 'end_date')
-    list_filter = ('teacher', 'start_date', 'end_date')
-    search_fields = ('title', 'description')
+    list_display = ('title', 'capacity', 'teacher')
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'phone')
+
+@admin.register(Enrollment)
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'enrolled_at')
